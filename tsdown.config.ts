615600fd504028e1a4ff20078cdd3f7217ab01bb
AQian <970506352@qq.com>
2025-12-18 18:15:34 +0800

    build: 引入tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig([
+  // for bundlers like vite, rollup, esbuild, webpack etc
+  {
+    entry: [
+      "src/**.ts",
+    ],
+    format: [
+      "esm",
+    ],
+    sourcemap: true,
+    dts: true,
+    outExtensions: ({ format }) => ({
+      js: format === "cjs" ? ".cjs" : ".mjs",
+    }),
+  },
+  // common js for node and other backend runtimes
+  {
+    entry: [
+      "src/index.ts",
+    ],
+    format: [
+      "cjs",
+    ],
+    sourcemap: true,
+    dts: true,
+    outExtensions: ({ format }) => ({
+      js: format === "cjs" ? ".cjs" : ".mjs",
+    }),
+  },
+  // bundle js for browsers/cdn
+  {
+    entry: {
+      bundle: "src/index.ts",
+    },
+    format: [
+      "esm",
+    ],
+    sourcemap: true,
+    dts: true,
+    outExtensions: ({ format }) => ({
+      js: format === "cjs" ? ".cjs" : ".mjs",
+    }),
+  },
+]);
