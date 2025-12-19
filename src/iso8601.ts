8fe06bdfb9320741ae3918b97f95df5006f99b70
AQian <970506352@qq.com>
2025-12-19 14:46:29 +0800

    feat: 添加iso8601验证

---
+++ src/iso8601.ts
@@ -0,0 +1,30 @@
+export const ISO8601_PATTERN =
+  /^([0-9]{4})-([0-1][0-9])(?:-([0-3][0-9]))?(?:[T ]?([0-2][0-9])(?::([0-5][0-9]))?(?::([0-5][0-9]))?)?(?:\.[0-9]+)?(Z|(?:\+|-)[0-9]{2}:?[0-9]{2})?$/;
+
+export const iso8601 = (date: string): boolean => {
+  const matches = date.match(ISO8601_PATTERN);
+  if (!matches) {
+    return false;
+  }
+
+  const month = Number(matches[2]);
+  if (month < 1 || month > 12) {
+    return false;
+  }
+
+  if (matches[3]) {
+    const day = Number(matches[3]);
+    if (day < 1 || day > 31) {
+      return false;
+    }
+  }
+
+  if (matches[4]) {
+    const hours = Number(matches[4]);
+    if (hours < 0 || hours > 23) {
+      return false;
+    }
+  }
+
+  return true;
+};
