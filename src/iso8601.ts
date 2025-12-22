32a7e7f443725ef8e981131872939ac84299c5d6
AQian <970506352@qq.com>
2025-12-22 14:47:07 +0800

    chore: 修改函数名使其更加语义化

--- src/iso8601.ts
+++ src/iso8601.ts
@@ -1,7 +1,7 @@
 export const ISO8601_PATTERN =
   /^([0-9]{4})-([0-1][0-9])(?:-([0-3][0-9]))?(?:[T ]?([0-2][0-9])(?::([0-5][0-9]))?(?::([0-5][0-9]))?)?(?:\.[0-9]+)?(Z|(?:\+|-)[0-9]{2}:?[0-9]{2})?$/;
 
-export const iso8601 = (date: string): boolean => {
+export const isIso8601 = (date: string): boolean => {
   const matches = date.match(ISO8601_PATTERN);
   if (!matches) {
     return false;
