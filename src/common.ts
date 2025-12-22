049ecb22057575a5652928575c135aafbabde330
AQian <970506352@qq.com>
2025-12-22 14:44:54 +0800

    feat: 添加字符串规范函数

--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,10 @@
 export const MS_DAY = 86400000 as const;
 
 export const SPEC_DATE = "1999-03-04T02:05:01.000Z" as const;
+
+export const normalizeStr = (part: Intl.DateTimeFormatPart): Intl.DateTimeFormatPart => {
+  if (part.type === "literal") {
+    part.value = part.value.normalize("NFKC");
+  }
+  return part;
+};
