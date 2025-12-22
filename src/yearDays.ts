def653c95033c0206be4a1f5eafb507f05c6c249
AQian <970506352@qq.com>
2025-12-22 10:16:47 +0800

    feat: 实现yearDays

---
+++ src/yearDays.ts
@@ -0,0 +1,12 @@
+import type { MaybeDateInput } from "./types";
+
+import { MS_DAY } from "./common";
+import { normalizeDate } from "./date";
+
+export const yearDays = (date?: MaybeDateInput): number => {
+  const d = normalizeDate(date);
+  return (
+    (new Date(d.getFullYear() + 1, 0, 0).getTime() - new Date(d.getFullYear(), 0, 0).getTime()) /
+    MS_DAY
+  );
+};
