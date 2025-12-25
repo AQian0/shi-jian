11c45d24191e03a26a82b33d116464ab7fbe4da0
AQian <970506352@qq.com>
2025-12-25 12:04:45 +0800

    style: 格式化代码

--- src/removeOffset.ts
+++ src/removeOffset.ts
@@ -6,4 +6,4 @@ import { FIRST_CHAR_INDEX } from "./common";
 export const removeOffset = (dateInput?: MaybeDateInput, offset = "+00:00"): Date => {
   const positive = offset.charAt(FIRST_CHAR_INDEX) === "+";
   return applyOffset(dateInput, offset.replace(positive ? "+" : "-", positive ? "-" : "+"));
-}
+};
