1ae1f4af3327c821091f592e618d4ce416e39b7b
AQian <970506352@qq.com>
2025-12-25 12:01:36 +0800

    chore: 修改为箭头函数

--- src/removeOffset.ts
+++ src/removeOffset.ts
@@ -3,7 +3,7 @@ import type { MaybeDateInput } from "./types";
 import { applyOffset } from "./applyOffset";
 import { FIRST_CHAR_INDEX } from "./common";
 
-export function removeOffset(dateInput?: MaybeDateInput, offset = "+00:00"): Date {
+export const removeOffset = (dateInput?: MaybeDateInput, offset = "+00:00"): Date => {
   const positive = offset.charAt(FIRST_CHAR_INDEX) === "+";
   return applyOffset(dateInput, offset.replace(positive ? "+" : "-", positive ? "-" : "+"));
 }
