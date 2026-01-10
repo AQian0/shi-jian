afe86ec664fcb2590ed1db7f1f3536a37f515156
AQian <970506352@qq.com>
2026-01-10 11:57:02 +0800

    ci: 忽略或修复oxlint报错

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,16 +4,18 @@ import DefaultTheme from "vitepress/theme";
 // https://vitepress.dev/guide/custom-theme
 import { h } from "vue";
 
+// oxlint-disable-next-line no-unassigned-import
 import "./style.css";
 
+// oxlint-disable-next-line no-default-export
 export default {
   extends: DefaultTheme,
-  Layout: () => {
+  Layout: (): ReturnType<typeof h> => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     });
   },
-  enhanceApp({ app, router, siteData }) {
+  enhanceApp(): void {
     // ...
   },
 } satisfies Theme;
